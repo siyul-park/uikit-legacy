ba5609acbc4c0bdef428b9e6fb138b585eb90483
siyual-park <siyual.bak@gmail.com>
2022-11-08 21:43:00 +0900

    fix: test

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,9 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'identity-obj-proxy',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '@type': '<rootDir>/src/type',
+    '@layout/(.*)$': '<rootDir>/src/layout/$1',
+    '@form/(.*)$': '<rootDir>/src/form/$1',
   },
   collectCoverage: true,
   coveragePathIgnorePatterns: [
